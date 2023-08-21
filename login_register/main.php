50967fe853762b98381b199ce90fb043f603594f
Yashasvi GM <132649643+Yashasvigm14@users.noreply.github.com>
2023-08-21 18:02:31 +0530

    Add files via upload

---
+++ login_register/main.php
@@ -0,0 +1,51 @@
+<!-- Banner -->
+<div>
+			<section class="banner full">
+				<article>
+					<img src="img/slider/slide01.jpg" alt="" />
+					<div class="inner">
+						<header>
+							<p>Discover the Best of collections available.</p>
+							<h2>COLORFUL SERENITY</h2>
+						</header>
+					</div>
+				</article>
+				<article>
+					<img src="img/slider/slide02.jpeg" alt="" />
+					<div class="inner">
+						<header>
+							<p>Get quality arts for best prices!</p>
+							<h2>ARTISTIC WONDERS</h2>
+						</header>
+					</div>
+				</article>
+				<article>
+					<img src="img/slider/slide03.jpeg"  alt="" />
+					<div class="inner">
+						<header>
+							<p>If you don’t love it, return it. It’s just that easy.</p>
+							<h2>OUR GUARANTEE</h2>
+						</header>
+					</div>
+				</article>
+				<article>
+					<img src="img/slider/slide04.jpg"  alt="" />
+					<div class="inner">
+						<header>
+							<p>Conversion of imagination to reality</p>
+							<h2>VIVID PORTRAYAL</h2>
+						</header>
+					</div>
+				</article>
+				<article>
+					<img src="img/slider/slide05.jpg"  alt="" />
+					<div class="inner">
+						<header>
+							<p>Fill your cart with art</p>
+							<h2>ART WORM</h2>
+						</header>
+					</div>
+				</article>
+			</section>
+
+			</div>
